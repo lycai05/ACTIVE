2d6291ac1be621c30e6c65982566dc2cbd33a623
ZhouQiJun <zhouqijun4job1@163.com>
2025-05-19 14:42:25 +0800

    format: 调整 script template 的顺序
    script 位于开头，更可读

--- src/views/Browse/human.vue
+++ src/views/Browse/human.vue
@@ -1,125 +1,3 @@
-<template>
-	<!-- <BrowserBase>
-    <template #default>
-      <ul>
-        <li class="flex space-x-3 items-center mb-2">
-          <svg class="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500" fill="currentColor"
-            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-              clip-rule="evenodd"></path>
-          </svg>
-          <p class="text-xl font-light text-gray-500">This is the Browser Navigation page for monkey.</p>
-        </li>
-      </ul>
-
-    </template>
-  </BrowserBase> -->
-	<n-card class="mb-6">
-		<div class="flex items-center justify-end mx-2 my-4">
-			<n-space>
-				<n-tooltip trigger="hover">
-					<template #trigger>
-						<n-button secondary size="small" @click="prepareScreenshot">
-							<template #icon>
-								<Icon icon="mdi:camera" />
-							</template>
-						</n-button>
-					</template>
-					Take screenshot
-				</n-tooltip>
-				<n-button secondary size="small" @click="showAddTrackModal = true">Add Track</n-button>
-				<n-button secondary size="small" @click="showTracksInfoModal = true">Show Tracks</n-button>
-				<n-tooltip trigger="hover">
-					<template #trigger>
-						<n-button secondary size="small" @click="handleRefresh">
-							<template #icon>
-								<Icon icon="mdi:refresh" />
-							</template>
-						</n-button>
-					</template>
-					Refresh the browser
-				</n-tooltip>
-				<n-tooltip trigger="hover">
-					<template #trigger>
-						<n-button secondary size="small" @click="openHelpPage">
-							<template #icon>
-								<Icon icon="mdi:help-circle" />
-							</template>
-						</n-button>
-					</template>
-					Open help page
-				</n-tooltip>
-			</n-space>
-		</div>
-		<ExpressoVue :key="refreshKey" id="human_browser" :assembly="asm" v-model:tracksInfo="tracksInfo"></ExpressoVue>
-	</n-card>
-	<!-- Screenshot Modal -->
-	<n-modal v-model:show="toggleComponent">
-		<n-card>
-			<template #header>Preview screenshot</template>
-			<ScreenshotExporter />
-			<n-divider />
-			<div ref="screenshotContainer" id="screenshot-container"></div>
-		</n-card>
-	</n-modal>
-	<!-- Modals -->
-	<n-modal
-		v-model:show="showAddTrackModal"
-		preset="card"
-		size="huge"
-		header-class="bg-white"
-		content-class="bg-white"
-	>
-		<template #header>Load tracks from file server</template>
-		<n-tabs type="line" animated>
-			<n-tab-pane name="Add Meta Track" tab="Meta Track" display-directive="if">
-				<DownloadTable :downloadFiles="downloadFiles_merged" @addTrack="addTrack"></DownloadTable>
-			</n-tab-pane>
-			<n-tab-pane name="Add Bulk 3D track" tab="Bulk Hi-C/HiChIP/ChIA-PET track" display-directive="show">
-				<ExpressoTable @add-track="addTrack"></ExpressoTable>
-			</n-tab-pane>
-			<n-tab-pane
-				name="Add Single cell 3D track"
-				tab="Single cell 3D track"
-				display-directive="show"
-			></n-tab-pane>
-			<n-tab-pane
-				name="Add Single molecular 3D track"
-				tab="Single molecular 3D track"
-				display-directive="show"
-			></n-tab-pane>
-		</n-tabs>
-	</n-modal>
-
-	<n-modal
-		v-model:show="showTracksInfoModal"
-		preset="card"
-		style="max-width: 900px"
-		header-class="bg-white"
-		content-class="bg-white"
-		size="huge"
-	>
-		<template #header>Loaded tracks</template>
-		<n-table :bordered="false" :single-line="false">
-			<thead>
-				<tr>
-					<th>Track Id</th>
-					<th>Track label</th>
-					<th>Track Type</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="track in tracksInfo" :key="track.id">
-					<td>{{ track.id }}</td>
-					<td>{{ track.name }}</td>
-					<td>{{ track.type }}</td>
-				</tr>
-			</tbody>
-		</n-table>
-	</n-modal>
-</template>
-
 <script setup>
 import { ref, computed, watch, nextTick } from 'vue'
 import BrowserBase from '@/components/browser/BrowserBase.vue'
@@ -457,6 +335,129 @@ const handleGeneSearch = async () => {
 //   tracksInfo.value = newTracks
 // }, {immediate: false})
 </script>
+
+<template>
+	<!-- <BrowserBase>
+    <template #default>
+      <ul>
+        <li class="flex space-x-3 items-center mb-2">
+          <svg class="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500" fill="currentColor"
+            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+              clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-xl font-light text-gray-500">This is the Browser Navigation page for monkey.</p>
+        </li>
+      </ul>
+
+    </template>
+  </BrowserBase> -->
+	<n-card class="mb-6">
+		<div class="flex items-center justify-end mx-2 my-4">
+			<n-space>
+				<n-tooltip trigger="hover">
+					<template #trigger>
+						<n-button secondary size="small" @click="prepareScreenshot">
+							<template #icon>
+								<Icon icon="mdi:camera" />
+							</template>
+						</n-button>
+					</template>
+					Take screenshot
+				</n-tooltip>
+				<n-button secondary size="small" @click="showAddTrackModal = true">Add Track</n-button>
+				<n-button secondary size="small" @click="showTracksInfoModal = true">Show Tracks</n-button>
+				<n-tooltip trigger="hover">
+					<template #trigger>
+						<n-button secondary size="small" @click="handleRefresh">
+							<template #icon>
+								<Icon icon="mdi:refresh" />
+							</template>
+						</n-button>
+					</template>
+					Refresh the browser
+				</n-tooltip>
+				<n-tooltip trigger="hover">
+					<template #trigger>
+						<n-button secondary size="small" @click="openHelpPage">
+							<template #icon>
+								<Icon icon="mdi:help-circle" />
+							</template>
+						</n-button>
+					</template>
+					Open help page
+				</n-tooltip>
+			</n-space>
+		</div>
+		<ExpressoVue :key="refreshKey" id="human_browser" :assembly="asm" v-model:tracksInfo="tracksInfo"></ExpressoVue>
+	</n-card>
+	<!-- Screenshot Modal -->
+	<n-modal v-model:show="toggleComponent">
+		<n-card>
+			<template #header>Preview screenshot</template>
+			<ScreenshotExporter />
+			<n-divider />
+			<div ref="screenshotContainer" id="screenshot-container"></div>
+		</n-card>
+	</n-modal>
+	<!-- Modals -->
+	<n-modal
+		v-model:show="showAddTrackModal"
+		preset="card"
+		size="huge"
+		header-class="bg-white"
+		content-class="bg-white"
+	>
+		<template #header>Load tracks from file server</template>
+		<n-tabs type="line" animated>
+			<n-tab-pane name="Add Meta Track" tab="Meta Track" display-directive="if">
+				<DownloadTable :downloadFiles="downloadFiles_merged" @addTrack="addTrack"></DownloadTable>
+			</n-tab-pane>
+			<n-tab-pane name="Add Bulk 3D track" tab="Bulk Hi-C/HiChIP/ChIA-PET track" display-directive="show">
+				<ExpressoTable @add-track="addTrack"></ExpressoTable>
+			</n-tab-pane>
+			<n-tab-pane
+				name="Add Single cell 3D track"
+				tab="Single cell 3D track"
+				display-directive="show"
+			></n-tab-pane>
+			<n-tab-pane
+				name="Add Single molecular 3D track"
+				tab="Single molecular 3D track"
+				display-directive="show"
+			></n-tab-pane>
+		</n-tabs>
+	</n-modal>
+
+	<n-modal
+		v-model:show="showTracksInfoModal"
+		preset="card"
+		style="max-width: 900px"
+		header-class="bg-white"
+		content-class="bg-white"
+		size="huge"
+	>
+		<template #header>Loaded tracks</template>
+		<n-table :bordered="false" :single-line="false">
+			<thead>
+				<tr>
+					<th>Track Id</th>
+					<th>Track label</th>
+					<th>Track Type</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="track in tracksInfo" :key="track.id">
+					<td>{{ track.id }}</td>
+					<td>{{ track.name }}</td>
+					<td>{{ track.type }}</td>
+				</tr>
+			</tbody>
+		</n-table>
+	</n-modal>
+</template>
+
 <style lang="scss" scoped>
 .compose-btn-wrap {
 	width: 20%;
