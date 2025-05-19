bd4c41294653bcc5d506e747bcd07eccff390978
ZhouQiJun <zhouqijun4job1@163.com>
2025-05-19 12:32:37 +0800

    format: 按照 prettier 的规则调整格式

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,21 +1,21 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-	import type { DefineComponent } from "vue"
+declare module '*.vue' {
+	import type { DefineComponent } from 'vue'
 	const component: DefineComponent<{}, {}, any>
 	export default component
 }
 
-declare module "*.svg" {
-	import type { DefineComponent } from "vue"
+declare module '*.svg' {
+	import type { DefineComponent } from 'vue'
 	const component: DefineComponent
 	export default component
 }
 
-declare module "vue-cal" {
-	import type { DefineComponent } from "vue"
+declare module 'vue-cal' {
+	import type { DefineComponent } from 'vue'
 	const component: DefineComponent
 	export default component
 }
 
-declare module "v-calendar"
+declare module 'v-calendar'
